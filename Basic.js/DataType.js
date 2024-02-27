c9f0c7b7eeb09883622839bb163a421fd29f9ca0
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2024-02-27 18:54:23 +0000

    container

---
+++ Basic.js/DataType.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer vrsion 
+
+// alert(3+3) we are using node js not browser
+
+let name="Nilesh"
+let age=19
+let isLoggin=false
+
+
+/*
+number =>2 to power 53
+bigint
+string=>""
+boolean=>true/false;
+null=>standalone value
+*/
