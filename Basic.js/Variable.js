3de42eff8a164503ff0c3dce395bcb86f65c03ac
neel-lsh <147874633+neel-lsh@users.noreply.github.com>
2024-02-27 18:17:32 +0000

    Container

---
+++ Basic.js/Variable.js
@@ -0,0 +1,14 @@
+const accountId= 708
+   // accountId=26 not allowed;
+   let accountEmail="nilesh@33Gmail"
+   var accountpass="316497"
+   coountcity="delhi"
+   /*VAR use nahi kiya jata because issue in block scope  and functional scope
+    */
+
+
+   accountEmail="bholu1166gmail"
+   accountpass="0456789"
+   accountcity="Muzaffarpur"
+   console.log(accountId);
+   console.table([accountEmail,accountId,accountpass,accountcity])
